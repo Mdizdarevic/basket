8fc2ac91ebe877e6f4dbdc254f8fe7ecd0e8bc4f
Moreno Dizdarevic <moreno.dizdarevic@gmail.com>
2026-06-30 14:07:53 +0200

    Secure Coding updates for github workflow

--- src/test/java/com/morenod/basket/SecurityIntegrationTests.java
+++ src/test/java/com/morenod/basket/SecurityIntegrationTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -13,6 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource(properties = "spring.main.allow-bean-definition-overriding=true")
 public class SecurityIntegrationTests {
 
     @Autowired
@@ -20,7 +22,7 @@ public class SecurityIntegrationTests {
 
     @Autowired
     private JwtUtil jwtUtil;
-    
+
     @Test
     void testPublicEndpointReturns200() throws Exception {
         mockMvc.perform(get("/api/donations")).andExpect(status().isOk());
